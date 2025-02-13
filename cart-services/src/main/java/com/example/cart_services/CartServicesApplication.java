dc3b73f2c76634cb715b4a256097b340b1c26f54
satyam <satyam98358@gmail.com>
2025-02-14 01:18:55 +1100

    adding cart services

---
+++ cart-services/src/main/java/com/example/cart_services/CartServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cart_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServicesApplication.class, args);
+	}
+
+}
