b1472ea52f75278089f8fbfd62285d91a56cc119
satyam <satyam98358@gmail.com>
2025-02-24 16:16:29 +1100

    Add API Gateway

---
+++ auth-services/src/test/java/com/example/auth_services/AuthServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
